1bcb9c2c0f3dd6821c1db8b96152391ed95e35c5
ale <ale@MacPro.local>
2011-07-06 02:24:28 +0200

    update to 7.4

--- themes/seven/page.tpl.php
+++ themes/seven/page.tpl.php
@@ -22,8 +22,8 @@
         <div id="help">
           <?php print render($page['help']); ?>
         </div>
-	  <?php endif; ?>
-	  <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+      <?php endif; ?>
+      <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
       <?php print render($page['content']); ?>
     </div>
 
